89ad4039cbdb065de3d27b65a97023ba1c4a05c2
kajal kukreja <gudukukreja16@gmail.com>
2021-08-31 01:29:18 +0530

    learn explore page

---
+++ public/index2.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.owl-carousel').owlCarousel2({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
